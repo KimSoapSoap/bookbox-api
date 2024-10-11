66c01c731072b57fc0eb1f8da22621c6be439d44
sudal1 <ckddbsdbs123@naver.com>
2024-10-11 14:45:18 +0900

    카테고리선택시책목록완료

--- src/main/java/green/mtcoding/bookbox/book/BookService.java
+++ src/main/java/green/mtcoding/bookbox/book/BookService.java
@@ -1,8 +1,11 @@
 package green.mtcoding.bookbox.book;
 
+import green.mtcoding.bookbox.category.Category;
+import green.mtcoding.bookbox.core.exception.api.ExceptionApi404;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,5 +21,33 @@ public class BookService {
 //        }
 //        List<Book> searchBookList = bookRepository.mFindAll(title, author, publisher);
 //    }
+    public List<BookResponse.BookListDTO> 메인책목록보기(){
+        List<Book> books = bookRepository.mFindAllWithCategory();
+        List<BookResponse.BookListDTO> dtos = new ArrayList<>();
+        for(Book book : books) {
+            BookResponse.BookListDTO dto = new BookResponse.BookListDTO(book);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
+    public List<BookResponse.CategoryDTO> 책과카테고리보기(){
+        List<Category> categories = bookRepository.mFindAllWithCategoryV2();
+        List<BookResponse.CategoryDTO> dtos = new ArrayList<>();
+        for(Category category : categories) {
+            BookResponse.CategoryDTO dto = new BookResponse.CategoryDTO(category);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    public List<BookResponse.clickCategoryDTO> 카테고리별책보기(String id){
+        List<Book> bookList = bookRepository.mFindByCategoryId(id);
+        List<BookResponse.clickCategoryDTO> dtos = new ArrayList<>();
+        for(Book book : bookList) {
+            BookResponse.clickCategoryDTO dto = new BookResponse.clickCategoryDTO(book);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 }
